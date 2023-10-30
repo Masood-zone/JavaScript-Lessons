9ee5100dacb4dabde619bb65ca778e335efa248f
Masood <masoodacheampong@gmail.com>
2023-10-30 22:26:24 +0000

    JavaScript beginner lessons part 1

---
+++ lesson-1.js
@@ -0,0 +1,17 @@
+// 2023 - ES-16 (A version of JavaScript that has become standardized)
+// Variables declarations
+// var firstName = "Masood"; //Oldest and outdated
+
+let firstName = "Masood"; // Currently used, can change it's value.
+const lastName = "Acheampong"; //Current used cannot change it value.
+
+// Changed value in firstName
+// lastName = "Sahada";
+
+// Output
+// console.log(firstName);
+// console.log(lastName);
+
+// Hoisting
+const price = "10";
+console.log(price);
---
+++ lesson-2.js
@@ -0,0 +1,42 @@
+// Data types
+// All the data types in JS
+// Values - Primitive
+true;
+false;
+1;
+("String");
+0.3424;
+12.34234;
+
+// Boolean - true or false
+let hasBrother = false;
+console.log(hasBrother);
+
+// null
+let noValue = null;
+console.log(noValue);
+
+//undefined
+let hasValue = undefined;
+hasValue = 1;
+console.log(hasValue);
+
+// Number
+let number = 121; //Value = number
+console.log(number);
+
+//String
+let stringNumber = "121"; // value = string
+console.log(stringNumber);
+console.log(number === stringNumber);
+
+// BigInt
+let largeValue = 3242985238534230934820385138902849048435803485n;
+console.log(largeValue);
+
+// Symbol
+let symbol1 = Symbol("2");
+let symbol2 = Symbol("2");
+console.log(symbol1 === symbol2);
+
+// Objects - Functions, Arrays and Objects Constructors, Classes
