0c20bca9f75d8722dff371747eb9a59f8efab271
Masood <masoodacheampong@gmail.com>
2023-11-03 21:51:21 +0000

    Assignment-1

---
+++ assignment.js
@@ -0,0 +1,18 @@
+// TODO - Create an array with 5 elements
+// <--- Array goes here --->
+
+// TODO - console.log(--yourArrayName--) your array to display it
+
+// TODO - I want you to push a new element into it.
+// Add any element you want
+
+// TODO - Remove a single item from the last position of the array
+// Use the pop() method
+
+// TODO - Add an item to your array's beginning
+
+// TODO - Remove an item from your array's beginnig
+
+// TODO - Create two arrays and concat them
+
+// TODO - Slice an array to get the first element to the last
